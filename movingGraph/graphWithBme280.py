9b2ea8fddd820d559b565b9bb1677b4cb2cba180
Merel <merelvandegraaf@gmail.com>
2020-07-20 16:54:24 +0200

    code for moving graph
    code for moving graph using bme280 temperature

--- movingGraph/graphWithBme280.py
+++ movingGraph/graphWithBme280.py
@@ -1,1 +1,47 @@
-yeet
+import datetime as dt
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import smbus2
+import bme280
+
+# Create figure for plotting
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+xs = []
+ys = []
+
+#initialize BME280
+port = 1
+address = 0x76
+bus = smbus2.SMBus(port)
+
+calibration_params = bme280.load_calibration_params(bus, address)
+
+# This function is called periodically from FuncAnimation
+def animate(i, xs, ys):
+
+    # Read temperature (Celsius) from BME280
+    data = bme280.sample(bus, address, calibration_params)
+    temp_c = round(data.temperature, 2)
+
+    # Add x and y to lists
+    xs.append(dt.datetime.now().strftime('%H:%M:%S.%f'))
+    ys.append(temp_c)
+
+    # Limit x and y lists to 20 items
+    xs = xs[-20:]
+    ys = ys[-20:]
+
+    # Draw x and y lists
+    ax.clear()
+    ax.plot(xs, ys)
+
+    # Format plot
+    plt.xticks(rotation=45, ha='right')
+    plt.subplots_adjust(bottom=0.30)
+    plt.title('TMP102 Temperature over Time')
+    plt.ylabel('Temperature (deg C)')
+
+# Set up plot to call animate() function periodically
+ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=1000)
+plt.show()
