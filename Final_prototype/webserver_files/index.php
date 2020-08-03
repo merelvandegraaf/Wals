ac6b964d058abb9705aa78a7bbd9e68be6167e23
merelvandegraaf <merelvandegraaf@gmail.com>
2020-08-03 19:21:48 +0200

    webserver files

---
+++ Final_prototype/webserver_files/index.php
@@ -0,0 +1,41 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "merel";
+$dbname = "wals_database";
+
+//create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+//check conn
+if($conn->connect_error){
+	die("connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM tempLog";
+$result = $conn->query($sql);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<Title><?php echo "Wals data"; ?></Title>
+<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<h1> <?php echo "Wals data"; ?></h1>
+<?php
+if ($result->num_rows > 0){
+	//output data
+	echo "<table> <tr><th>GPS lon</th><th>GPS lat</th><th>air temp</th><th>air pressure</th>
+	<th>air humidity</th><th>temp</th></tr>";
+	while($row = $result->fetch_assoc()){
+		echo "<tr><td>" . $row[GPS_lon]. "</td><td>" . $row[GPS_lat]. 
+		"</td><td>" . $row[air_temp]. "</td><td>" . $row[air_pressure]. 
+		"</td><td>" . $row[air_humidity]. "</td><td>" . $row[temp]."</td></tr>";
+	}
+	echo "</table>";
+} else{
+	echo "0 results";
+}
+$conn->close();
+?>
+</body></html>
