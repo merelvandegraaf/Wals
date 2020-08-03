a1cbd1a2dfc15677d500485b0ebc4c2e05cbd35d
Merel <merelvandegraaf@gmail.com>
2020-08-03 16:54:54 +0200

    empty table and fix exit

--- Final_prototype/data_to_SQL.py
+++ Final_prototype/data_to_SQL.py
@@ -3,6 +3,7 @@ import bme280
 import MySQLdb
 import time
 
+running = True
 #params for bme)280
 port = 1
 address = 0x76
@@ -13,24 +14,32 @@ calibration_params = bme280.load_calibration_params(bus, address)
 db = MySQLdb.connect(host="localhost", user="root",passwd="merel", db="wals_database")
 cur = db.cursor()
 
-while True:
-    data = bme280.sample(bus, address, calibration_params)
-    sql = ("""INSERT INTO tempLog (GPS_lon,GPS_lat,air_temp,air_pressure,air_humidity,temp) VALUES (%s,%s,%s,%s,%s,%s)""", (0, 0, data.temperature, data.pressure, data.humidity, 0))
-    try:
-        print("Writing to database...")
-        # Execute the SQL command
-        cur.execute(*sql)
-        # Commit your changes in the database
-        db.commit()
-        print("Write Complete")
+sql = ("""DELETE FROM tempLog""")
+try:
+    cur.execute(*sql)
+    db.commit()
+except:
+    db.rollback()
 
-    except:
-        # Rollback in case there is any error
-        db.rollback()
-        print("Failed writing to database")
+try:
+    while running:
+        data = bme280.sample(bus, address, calibration_params)
+        sql = ("""INSERT INTO tempLog (GPS_lon,GPS_lat,air_temp,air_pressure,air_humidity,temp) VALUES (%s,%s,%s,%s,%s,%s)""", (0, 0, data.temperature, data.pressure, data.humidity, 0))
+        try:
+            print("Writing to database...")
+            # Execute the SQL command
+            cur.execute(*sql)
+            # Commit your changes in the database
+            db.commit()
+            print("Write Complete")
+        except:
+         # Rollback in case there is any error
+            db.rollback()
+            print("Failed writing to database")
+        time.sleep(10)
 
-    time.sleep(10)
-    
-cur.close()
-db.close()
+except (KeyboardInterrupt):
+    running = False
+    cur.close()
+    db.close()
 
