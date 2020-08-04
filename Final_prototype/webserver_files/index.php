613d6898c68e027f01733512ae9f59dbe17d985a
Merel <merelvandegraaf@gmail.com>
2020-08-04 14:10:28 +0200

    werk please

--- Final_prototype/webserver_files/index.php
+++ Final_prototype/webserver_files/index.php
@@ -14,30 +14,53 @@ if($conn->connect_error){
 $sql = "SELECT * FROM tempLog";
 $result = $conn->query($sql);
 
-function exportDatabase($setRec){
-	$columnHeader = '';  
-$columnHeader = "User Id" . "\t" . "First Name" . "\t" . "Last Name" . "\t" 
- . "Test" . "\t" . "test 2" . "\t"  . "test 3" . "\t";  
-$setData = '';  
-  while ($rec = mysqli_fetch_row($setRec)) {  
-    $rowData = '';  
-    foreach ($rec as $value) {  
-        $value = '"' . $value . '"' . "\t";  
-        $rowData .= $value;  
-    }  
-    $setData .= trim($rowData) . "\n";  
-}  
-  
-header("Content-type: application/octet-stream");  
-header("Content-Disposition: attachment; filename=User_Detail.xls");  
-header("Pragma: no-cache");  
-header("Expires: 0");  
+function exportDatabase(){
 
-  echo ucwords($columnHeader) . "\n" . $setData . "\n";  
+$sql = "SELECT * FROM tempLog";
+$results = $conn->query($sql);
+	
+$rows = results->fetch_assoc();
+ 
+ //The name of the Excel file that we want to force the
+//browser to download.
+$filename = 'members.xls';
+ 
+//Send the correct headers to the browser so that it knows
+//it is downloading an Excel file.
+header("Content-Type: application/xls");    
+header("Content-Disposition: attachment; filename=$filename");  
+header("Pragma: no-cache"); 
+header("Expires: 0");
+
+//Define the separator line
+$separator = "\t";
+ 
+//If our query returned rows
+if(!empty($rows)){
+    
+    //Dynamically print out the column names as the first row in the document.
+    //This means that each Excel column will have a header.
+    echo implode($separator, array_keys($rows[0])) . "\n";
+    
+    //Loop through the rows
+    foreach($rows as $row){
+        
+        //Clean the data and remove any special characters that might conflict
+        foreach($row as $k => $v){
+            $row[$k] = str_replace($separator . "$", "", $row[$k]);
+            $row[$k] = preg_replace("/\r\n|\n\r|\n|\r/", " ", $row[$k]);
+            $row[$k] = trim($row[$k]);
+        }
+        
+        //Implode and print the columns out using the 
+        //$separator as the glue parameter
+        echo implode($separator, $row) . "\n";
+    }
+}
 }
 
 if(isset($_POST[export])){
-exportDatabase($result);
+exportDatabase();
 }
 
 ?>
